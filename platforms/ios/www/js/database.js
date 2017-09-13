b1ee30e7ac10c4e65da7d4ef7ebbda7bf2d5eb58
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2017-09-13 15:29:49 -0500

    Icono de aplicación

--- platforms/ios/www/js/database.js
+++ platforms/ios/www/js/database.js
@@ -9,97 +9,97 @@ var db = null;
 
 
 function conexionDB(){
-	 db = window.openDatabase('odoo.db', '1.0', '', 1);
+    db = window.openDatabase('odoo.db', '1.0', '', 1);
 }
 
 
 
-function tablasInicialesDB(){ 
-  // En caso de que no existan las tablas en la base de datos, este proceso es el encargado de crearlas.
-  
-  // ========== TABLA PARA MANTENER EL USUARIO LOGUEADO ========== //
-   // db.transaction(function (tx) {
-   //    tx.executeSql('DROP TABLE usuario');
-   //  });
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS usuario (uid, email_login, pass_login, ip_login, db, id_empleado)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  }); 
+function tablasInicialesDB(){
+    // En caso de que no existan las tablas en la base de datos, este proceso es el encargado de crearlas.
+
+    // ========== TABLA PARA MANTENER EL USUARIO LOGUEADO ========== //
+    // db.transaction(function (tx) {
+    //    tx.executeSql('DROP TABLE usuario');
+    //  });
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS usuario (uid, email_login, pass_login, ip_login, db, id_empleado)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
 
 
-  // ========== TABLA PARA CREAR LOS PAGOS TEMPORALES ========== //
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS pagos (id, id_orden, cobrador, secuencia, fecha, cantidad)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  }); 
+    // ========== TABLA PARA CREAR LOS PAGOS TEMPORALES ========== //
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS pagos (id, id_orden, cobrador, secuencia, fecha, cantidad)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
 
 
-  // ========== TABLA PARA CREAR LOS PRESTAMOS TEMPORALES ========== //
+    // ========== TABLA PARA CREAR LOS PRESTAMOS TEMPORALES ========== //
     // db.transaction(function (tx) {
     //   tx.executeSql('DROP TABLE prestamos');
     // });
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS prestamos (id, zona, cliente, valor_prestamo, interes, cuotas, fecha)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  }); 
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS prestamos (id, zona, cliente, valor_prestamo, interes, cuotas, fecha)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
 
 
-  // ========== TABLA PARA CREAR LOS GASTOS TEMPORALES ========== //
+    // ========== TABLA PARA CREAR LOS GASTOS TEMPORALES ========== //
     // db.transaction(function (tx) {
     //   tx.executeSql('DROP TABLE prestamos');
     // });
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS gastos (id, zona, valor, fecha, razon)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  }); 
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS gastos (id, zona, valor, fecha, razon)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
 
-  // ========== TABLA PARA CREAR LOS CLIENTES TEMPORALES ========== //
+    // ========== TABLA PARA CREAR LOS CLIENTES TEMPORALES ========== //
     // db.transaction(function (tx) {
     //   tx.executeSql('DROP TABLE prestamos');
     // });
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS clientes (id, nombre, zona, direccion, telefono, movil)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  });
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS clientes (id, nombre, zona, direccion, telefono, movil)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
 
 
-  // ========== TABLA PARA CREAR LAS ZONAS TEMPORALES ========== //
+    // ========== TABLA PARA CREAR LAS ZONAS TEMPORALES ========== //
     // db.transaction(function (tx) {
     //   tx.executeSql('DROP TABLE prestamos');
     // });
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS zonas (id, nombre, pais, encargado, capital, caja)');
-  }, function(error) {
-      myApp.hidePreloader();
-      myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
-      console.log('Transaction ERROR: ' + error.message);
-  }, function() {
-      //myApp.alert('Tablas creadas', 'Sucess');
-  }); 
-   
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS zonas (id, nombre, pais, encargado, capital, caja)');
+    }, function(error) {
+        myApp.hidePreloader();
+        myApp.alert('Ha ocurrido un error en la base de datos', 'Error');
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        //myApp.alert('Tablas creadas', 'Sucess');
+    });
+
 }
